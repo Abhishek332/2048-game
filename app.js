1b34f8c956352aba39584e692743d6643dfedd36
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-01-20 18:52:28 +0530

    Left Right Functionality Complete, Remaining Combine column and up down move

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else randomNumberGenerator();
   }
 
-  //for move left to right
+  //for Left - Right move
   function moveLeftRight(val) {
     for (let i = 0; i < width * width; i++) {
       if (i % width == 0) {
@@ -59,5 +59,61 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
-  moveLeftRight("left");
+
+  //combine Row
+  function combineRow() {
+    for (let i = 0; i < width * width - 1; i++) {
+      if (
+        parseInt(Buttons[i].innerHTML) == parseInt(Buttons[i + 1].innerHTML)
+      ) {
+        Buttons[i + 1].innerHTML =
+          parseInt(Buttons[i].innerHTML) + parseInt(Buttons[i + 1].innerHTML);
+        Buttons[i].innerHTML = 0;
+      }
+    }
+  }
+
+  //combine Column
+  function combineCol() {}
+
+  //Button Click Call
+  function InputControl(e) {
+    if (e.keyCode === 37) {
+      keyLeftRightUpDown("left");
+      return;
+    }
+    if (e.keyCode === 38) {
+      keyLeftRightUpDown("up");
+      return;
+    }
+    if (e.keyCode === 39) {
+      keyLeftRightUpDown("right");
+      return;
+    }
+    if (e.keyCode === 40) {
+      keyLeftRightUpDown("down");
+      return;
+    }
+  }
+  document.addEventListener("keyup", InputControl);
+
+  //Left - Right, Up - Down Button Control
+  function keyLeftRightUpDown(val) {
+    val == "left" || val === "right" ? moveLeftRight(val) : moveUpDown(val);
+    val == "left" || val === "right" ? combineRow() : combineCol();
+    val == "left" || val === "right" ? moveLeftRight(val) : moveUpDown(val);
+    randomNumberGenerator();
+  }
+
+  //for Up - Down Move
+  function moveUpDown(val) {
+    let column = Array(width * width).fill(0);
+    for (let i = 0; i < width * width; i++) {
+      let count = 0;
+      // column[i / width] = parseInt(Buttons[(i % width) * width + i].innerHTML);
+      console.log((i % width) * width + i);
+    }
+    console.log("Column", column);
+  }
+  // moveUpDown();
 });
